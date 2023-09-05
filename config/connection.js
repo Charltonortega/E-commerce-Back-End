89ab84b2d2b818d8f8fb88daad0765719cdf70aa
Charlton Ortega <charltonortega14@gmail.com>
2023-09-05 13:49:09 +0930

    database and connection creation

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3301,
+    define: {
+      timestamps: false,
+    },
+  }
+);
 
 module.exports = sequelize;
